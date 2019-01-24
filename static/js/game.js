9f710db06f644ce321cc2af23fd9730f72fc78d2
Erika Toth <erikatoth.0831@gmail.com>
2019-01-24 14:31:23 +0100

    game ends correctly

--- static/js/game.js
+++ static/js/game.js
@@ -2,27 +2,30 @@ let currentPlayer = 1;
 let gameSpace = document.getElementById('game-space');
 const cards = JSON.parse(gameSpace.dataset.cards);
 const imagePath = gameSpace.dataset.imagePath;
+let gameActive = false;
 let deck;
 gameSpace.addEventListener('click', gameClick);
 gameSpace.addEventListener("mouseover", gameHover);
 
 function gameClick(event) {
-    if (event.target.classList.contains('btn-primary') && event.target.classList.contains('draw-card')) {
-        drawCard(currentPlayer);
-        if (playerLoseCheck(currentPlayer)) {
-            playerLost(currentPlayer);
-        }
-    } else if (event.target.id == 'button-new-game') {
+    if (event.target.id == 'button-new-game') {
         startNewGame();
-    } else if (event.target.classList.contains('bet-btn')) {
-        document.getElementById('bet-box').setAttribute("value", event.target.dataset.value);
-    } else if (event.target.classList.contains('bet')) {
-        let retchips = JSON.parse(localStorage.getItem('countchips'));
-        retchips['chips'] = retchips['chips'] - document.getElementById('bet-box').value;
-        document.getElementById('chips-left').innerHTML = retchips['chips'];
-    } else if (event.target.classList.contains('btn-primary') && event.target.classList.contains('stop-drawing')) {
-        stopDrawing();
-        handleBank();
+    } else if (gameActive) {
+        if (event.target.classList.contains('btn-primary') && event.target.classList.contains('draw-card')) {
+            drawCard(currentPlayer);
+            if (playerLoseCheck(currentPlayer)) {
+                playerLost(currentPlayer);
+            }
+        } else if (event.target.classList.contains('bet-btn')) {
+            document.getElementById('bet-box').setAttribute("value", event.target.dataset.value);
+        } else if (event.target.classList.contains('bet')) {
+            let retchips = JSON.parse(localStorage.getItem('countchips'));
+            retchips['chips'] = retchips['chips'] - document.getElementById('bet-box').value;
+            document.getElementById('chips-left').innerHTML = retchips['chips'];
+        } else if (event.target.classList.contains('btn-primary') && event.target.classList.contains('stop-drawing')) {
+            stopDrawing();
+            handleBank();
+        }
     }
 }
 
@@ -69,6 +72,7 @@ function startNewGame() {
     let retchips = JSON.parse(localStorage.getItem('countchips'));
     document.getElementById('chips-left').innerHTML = retchips['chips'];
     console.log('retchips: ', retchips);
+    gameActive = true;
 }
 
 function countPoints(playerId) {
@@ -101,6 +105,7 @@ function handleBank() {
 
 function playersWin() {
     alert('Player wins');
+    gameActive = false;
 }
 
 function checkWinners() {
@@ -125,6 +130,7 @@ function playerLoseCheck(player) {
 
 function playerLost(player) {
     alert('Player lost');
+    gameActive = false;
 }
 
 
@@ -136,8 +142,6 @@ function startingChips(currentPlayer) {
 
 function countingChips(currentPlayer) {
     let bet = document.getElementById('bet-box').value;
-
-
 }
 
 
@@ -148,4 +152,5 @@ function countPlayerCards(player) {
 
 function playerWins(player) {
     alert('Player won!');
+    gameActive = false;
 }
