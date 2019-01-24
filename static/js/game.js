93777c6beceb0a57856cf489f0ef488b940b0b53
Erika Toth <erikatoth.0831@gmail.com>
2019-01-24 14:16:51 +0100

    handle bank

--- static/js/game.js
+++ static/js/game.js
@@ -9,8 +9,14 @@ gameSpace.addEventListener("mouseover", gameHover);
 function gameClick(event) {
     if (event.target.classList.contains('btn-primary') && event.target.classList.contains('draw-card')) {
         drawCard(currentPlayer);
+        if (playerLoseCheck(currentPlayer)) {
+            playerLost(currentPlayer);
+        }
     } else if (event.target.id == 'button-new-game') {
         startNewGame();
+    } else if (event.target.classList.contains('btn-primary') && event.target.classList.contains('stop-drawing')) {
+        stopDrawing();
+        handleBank();
     }
 }
 
@@ -24,7 +30,7 @@ function drawCard(currentPlayer) {
     const cardPosition = Math.floor(Math.random() * deck.length);
     const cardId = deck[cardPosition];
     deck.splice(cardPosition, 1);
-    console.log(deck, cardId);
+    // console.log(deck, cardId);
     const card = getCardById(cardId);
     const img = document.createElement('img');
     img.classList.add('game-card');
@@ -62,6 +68,61 @@ function countPoints(playerId) {
     for (let card of cards) {
         pointCount += Number(card.dataset.value);
     }
-    console.log(`#player-hand${playerId}`);
-    document.getElementById(`player-hand${playerId}`).title = pointCount.toString();
+    // console.log(`#player-hand${playerId}`);
+    return document.getElementById(`player-hand${playerId}`).title = pointCount.toString();
+}
+
+function stopDrawing() {
+    let scoreCurrentPlayer = Number(countPoints(1));
+    let scorePlayer0 = Number(countPoints(0));
+}
+
+function handleBank() {
+    let highestPlayerScore = Number(countPoints(1));
+    let bankScore = Number(countPoints(0));
+    while (bankScore <= highestPlayerScore && bankScore !== 21) {
+        drawCard(0);
+        bankScore = Number(countPoints(0));
+    }
+    if (bankScore > 21) {
+        playersWin();
+    } else {
+        checkWinners();
+    }
+}
+
+function playersWin() {
+    alert('Player wins');
+}
+
+function checkWinners() {
+    let playerScore = Number(countPoints(1));
+    let bankScore = Number(countPoints(0));
+    if (bankScore < playerScore) {
+        playerWins(1);
+    } else if (bankScore === playerScore) {
+        if (countPlayerCards(0) >= countPlayerCards(1)) {
+            playerWins(1);
+        } else {
+            playerLost(1);
+        }
+    } else {
+        playerLost(1);
+    }
+}
+
+function playerLoseCheck(player) {
+    return Number(countPoints(player)) > 21;
+}
+
+function playerLost(player) {
+    alert('Player lost');
+}
+
+function countPlayerCards(player) {
+    return document.getElementById(`player-hand${player}`).getElementsByTagName('img').length;
+}
+
+function playerWins(player) {
+    alert('Player won!');
 }
